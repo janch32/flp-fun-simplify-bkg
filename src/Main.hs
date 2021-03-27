02d6df79cc3a3fd40f42108803c58b1bcd038b63
Jan Chaloupka <osobni@janchaloupka.cz>
2021-03-27 03:33:37 +0100

    new

--- src/Main.hs
+++ src/Main.hs
@@ -1,37 +1,32 @@
 module Main where
 
-import System.Console.GetOpt
 import System.Environment
 import System.Exit
 
-data Flag = ListLoaded | FirstStep | SimplifyFull deriving(Show)
-
-options :: [OptDescr Flag]
-options =
-    [
-        Option ['i'] [] (NoArg ListLoaded) "Show loaded BKG as internal representation",
-        Option ['1'] [] (NoArg FirstStep) "Print BKG after first step of a simplify algorithm",
-        Option ['2'] [] (NoArg SimplifyFull) "Print fully simplified BKG"
-    ]
-
 -- TODO předělat aby to vypadalo normálně
-parseOpts :: [String] -> IO (Flag, String)
-parseOpts argv =
-    let header = "Usage: simplify-bkg [-i12] file"
-    in case getOpt Permute options argv of
-        ([], _, []) -> die (usageInfo header options)
-        ([f], [n], []) -> return (f,n)
-        (_, _, []) -> ioError (userError "Wrong number of arguments")
-        (_, _, errs) -> ioError (userError (concat errs))
+--parseOpts argv =
+--    let header = "Usage: simplify-bkg [-i12] file"
+--    in case getOpt Permute options argv of
+--        ([], _, []) -> die (usageInfo header options)
+--        ([f], [n], []) -> return (f,n)
+--        (_, _, []) -> ioError (userError "Wrong number of arguments")
+--        (_, _, errs) -> ioError (userError (concat errs))
+
+data Flag = Loaded | FirstStep | Full
+data Opt = Opt Flag String | None
+
+parseOpts :: [String] -> Opt
+parseOpts ["-i", file] = Opt Loaded file
+parseOpts ["-1", file] = Opt FirstStep file
+parseOpts ["-2", file] = Opt Full file
+parseOpts s = None
 
 main = do
     argv <- getArgs
-    opts <- parseOpts argv
-    print opts
+    opt <- parseOpts argv
+    simplifyBkg opt
 
-
-simplifyBkg ListLoaded = print "List loaded"
-
-simplifyBkg FirstStep = print "First step"
-
-simplifyBkg SimplifyFull = print "simplify full"
+simplifyBkg :: Opt -> IO ()
+simplifyBkg (Opt Loaded file) = print "print loaded"
+simplifyBkg (Opt FirstStep file) = print "first step"
+simplifyBkg (Opt Full file) = print "full"
